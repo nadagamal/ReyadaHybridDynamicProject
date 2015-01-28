dd2931f530aa8707d579aa56b4f6998cd3ef6edf
Mohamed Mansour <sphone.developer@gmail.com>
2015-01-28 11:56:02 +0200

    Initial Commit

---
+++ HybridDynamicProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridDynamicProject
+//
+//  Created by Mohamed Mansour on 1/28/15.
+//  Copyright (c) 2015 Mohamed Mansour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HybridDynamicProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HybridDynamicProject
+//
+//  Created by Mohamed Mansour on 1/28/15.
+//  Copyright (c) 2015 Mohamed Mansour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
